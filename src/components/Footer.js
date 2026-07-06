0e4c0265a527acf4a0ea7b185e32e899bdf65b6e
Hun <92499881+crimama@users.noreply.github.com>
2026-07-07 00:15:53 +0900

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function Footer() {
-
   return (
     <div className='footer'>
-        <div className='footer__text'>Designed by Youngeun Suh</div>
-        <div className='footer__text'>© 2024 All rights reserved by Youngeun Suh.</div>
+      <div className='footer__text'>임훈 ♥ 오윤경</div>
+      <div className='footer__text'>2026.11.28 서울대학교 연구공원 웨딩홀</div>
+      <div className='footer__text footer__credit'>Based on YOUNGEUN100/react-wedding-card</div>
     </div>
   )
 }
