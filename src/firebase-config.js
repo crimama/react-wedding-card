a25fa15afa97446a9199d5159b36d6588698de71
Hun <92499881+crimama@users.noreply.github.com>
2026-07-07 01:00:19 +0900

    Point guestbook to Hun Yunkyung Firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,22 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDHaoVL3mi4_PFRxbr91cQOJ8SvRkqNbLg",
-  authDomain: "wedding-invitation-5e3c5.firebaseapp.com",
-  projectId: "wedding-invitation-5e3c5",
-  storageBucket: "wedding-invitation-5e3c5.appspot.com",
-  messagingSenderId: "288046083919",
-  appId: "1:288046083919:web:7621d047d94c49d8059f3a",
-  measurementId: "G-KP677M2TTQ"
+  apiKey: "AIzaSyCnPihKAD3oTQmk5--lmuyib1VG7gWDgGY",
+  authDomain: "hun-yunkyung-wedding.firebaseapp.com",
+  projectId: "hun-yunkyung-wedding",
+  storageBucket: "hun-yunkyung-wedding.firebasestorage.app",
+  messagingSenderId: "696038345423",
+  appId: "1:696038345423:web:c1ac1ee029c1b5ad0645ba"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
