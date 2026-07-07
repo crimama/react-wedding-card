22ef7f2a46b3bda4d431f31317e6c3887a917c1b
Hun <92499881+crimama@users.noreply.github.com>
2026-07-07 10:18:38 +0900

    Update cover layout with invitation-style hero

--- src/pages/Cover.js
+++ src/pages/Cover.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import mainPhoto from '../images/wedding-cover.jpg'
-import { GoHeartFill } from "react-icons/go";
 
 function Cover() {
 
   return (
-    <div className="container">
-      <div className='title'>&ldquo;우리 결혼합니다&rdquo;</div>
-      <img className="cover__main-photo" src={mainPhoto} alt='임훈 오윤경 웨딩사진' />
-      <div className='cover__person'>
-        <div>임훈</div>
-        <GoHeartFill className='cover__icon-heart' size="0.8em"/>
-        <div>오윤경</div>
+    <div className="container cover">
+      <div className="cover__frame">
+        <div className="cover__image-wrap">
+          <img className="cover__main-photo" src={mainPhoto} alt='임훈 오윤경 웨딩사진' />
+        </div>
+        <div className="cover__content">
+          <div className="cover__date-mark">11.28</div>
+          <div className="cover__time">SAT PM 5:00</div>
+          <div className='cover__place'>서울대학교 연구공원 웨딩홀</div>
+          <div className='cover__person'>
+            <span>임훈</span>
+            <span>오윤경</span>
+          </div>
+        </div>
       </div>
-      <div className='cover__date'>2026년 11월 28일 토요일 오후 5시</div>
-      <div className='cover__place'>서울대학교 연구공원 웨딩홀</div>
-      <div className='cover__line'></div>
     </div>
   )
 }
