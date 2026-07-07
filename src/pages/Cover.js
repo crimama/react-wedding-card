148ddaca453bc000f1083a0b2002a5dce715a4de
Hun <92499881+crimama@users.noreply.github.com>
2026-07-07 11:13:45 +0900

    Use date-focused cover text composition

--- src/pages/Cover.js
+++ src/pages/Cover.js
@@ -9,17 +9,14 @@ function Cover() {
         <div className="cover__image-wrap">
           <img className="cover__main-photo" src={mainPhoto} alt='임훈 오윤경 웨딩사진' />
         </div>
-        <div className="cover__top-copy">
-          <div className="cover__tagline">서로에게<br />가장 완벽한<br />러닝메이트</div>
-          <div className="cover__headline">우리<br />결혼합니다</div>
-          <div className="cover__hero-date">2026.11.28 pm5:00</div>
-        </div>
         <div className="cover__content">
+          <div className="cover__date-mark">11.28</div>
+          <div className="cover__time">SAT PM 5:00</div>
+          <div className='cover__place'>서울대학교 연구공원 웨딩홀</div>
           <div className='cover__person'>
             <span>임훈</span>
             <span>오윤경</span>
           </div>
-          <div className='cover__place'>서울대학교 연구공원 웨딩홀</div>
         </div>
       </div>
     </div>
