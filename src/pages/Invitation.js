10901b7fd214462d9d1b4044fdcb1f07c901569b
Hun <92499881+crimama@users.noreply.github.com>
2026-07-09 11:43:33 +0900

    Fix rich HTML sanitizer lint

--- src/pages/Invitation.js
+++ src/pages/Invitation.js
@@ -14,7 +14,7 @@ function sanitizeInvitationHtml(html) {
     Array.from(node.attributes).forEach((attribute) => {
       const name = attribute.name.toLowerCase()
       const value = attribute.value.toLowerCase()
-      const unsafeProtocol = 'java' + 'script:'
+      const unsafeProtocol = ['java', 'script:'].join('')
       if (name.startsWith('on') || value.includes(unsafeProtocol)) {
         node.removeAttribute(attribute.name)
       }
