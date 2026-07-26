c4c08695663382e192196679410e4188b02a1471
Hun <92499881+crimama@users.noreply.github.com>
2026-07-26 10:14:19 +0900

    Use Firestore only for gallery uploads

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCnPihKAD3oTQmk5--lmuyib1VG7gWDgGY",
@@ -13,7 +12,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storage = getStorage(app);
 
-export { storage };
 export default db;
